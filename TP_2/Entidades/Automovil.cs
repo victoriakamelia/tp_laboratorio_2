1ad2f1247c414a0f9fa3c7430503365465f5d62b
Victoria Pereyra <vpereyra@prismamp.com>
2020-05-10 22:41:50 -0300

    TP 2

---
+++ TP_2/Entidades/Automovil.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Clase derivada de Vehiculo.
+    /// </summary>
+    public class Automovil : Vehiculo
+    {
+        public enum ETipo 
+        { 
+            Monovolumen, Sedan 
+        }
+
+        private ETipo tipo;
+
+        /// <summary>
+        /// ReadOnly: retornará tamaño del Automovil.
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de la clase Automovil.
+        /// </summary>
+        /// <param name="marca">Marca del Automovil.</param>
+        /// <param name="chasis">Chasis del Automovil.</param>
+        /// <param name="color">Color del Automovil.</param>
+        /// <param name="tipo"><Tipo del Automovil./param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color, ETipo tipo): this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Constructor de la clase Automovil.
+        /// Por defecto, "tipo" será Monovolumen.
+        /// </summary>
+        /// <param name="marca">Marca del Automovil.</param>
+        /// <param name="chasis">Chasis del Automovil.</param>
+        /// <param name="color">Color del Automovil.</param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color): base (chasis, marca, color)
+        {
+            this.tipo = ETipo.Monovolumen;
+        }
+
+        /// <summary>
+        /// Muestra los atributos de un Automovil.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO: {0}\n", this.Tamanio);
+            sb.AppendFormat("TIPO  : {0}",this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP_2/Entidades/Vehiculo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda
+        }
+
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        private string chasis;
+        private ConsoleColor color;
+        private EMarca marca;
+        
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño.
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+
+        /// <summary>
+        /// Constructor de la clase.
+        /// </summary>
+        /// <param name="chasis">Recibe el valor a guardar en el atributo "chasis".</param>
+        /// <param name="marca">Recibe el valor a guardar en el atributo "marca"</param>
+        /// <param name="color">Recibe el valor a guardar en el atributo "color"</param>
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns>Retornará un srting con los datos del Vehiculo.</returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        /// <summary>
+        /// Recibe un vehículo cuyos atributos serán devueltos por string.
+        /// </summary>
+        /// <param name="unVehiculo">Vehiculo a mostrar.</param>
+        public static explicit operator string(Vehiculo unVehiculo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CHASIS: {0}\r\n", unVehiculo.chasis);
+            sb.AppendFormat("MARCA : {0}\r\n", unVehiculo.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\r\n", unVehiculo.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis.
+        /// </summary>
+        /// <param name="vehiculoUno">Primer Vehiculo a comparar.</param>
+        /// <param name="vehiculoDos">Segundo Vehiculo a comparar.</param>
+        /// <returns>Bool informando si los vehículos son o no iguales.</returns>
+        public static bool operator ==(Vehiculo vehiculoUno, Vehiculo vehiculoDos)
+        {
+            return (vehiculoUno.chasis == vehiculoDos.chasis);
+        }
+
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto.
+        /// </summary>
+        /// <param name="vehiculoUno">Primer Vehiculo a comparar</param>
+        /// <param name="vehiculoDos">Segundo Vehiculo a comparar.</param>
+        /// <returns>Bool informando si los vehículos son o no distintos.</returns>
+        public static bool operator !=(Vehiculo vehiculoUno, Vehiculo vehiculoDos)
+        {
+            return !(vehiculoUno == vehiculoDos);
+        }
+    }
+}
